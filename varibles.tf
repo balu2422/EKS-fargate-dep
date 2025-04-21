f8f9bb86e570561106ce3ecacc928e28f49aa24e
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-22 00:33:34 +0530

    Update varibles.tf

--- varibles.tf
+++ varibles.tf
@@ -4,7 +4,7 @@ variable "region" {
 }
 
 variable "vpc_name" {
-  description = "The name of the VPC"
+  description = "Name of the VPC"
   type        = string
 }
 
@@ -14,41 +14,51 @@ variable "vpc_cidr" {
 }
 
 variable "az1" {
-  description = "Availability Zone 1"
+  description = "First Availability Zone"
   type        = string
 }
 
 variable "az2" {
-  description = "Availability Zone 2"
+  description = "Second Availability Zone"
   type        = string
 }
 
 variable "public_subnet_az1_cidr" {
-  description = "CIDR block for public subnet in AZ1"
+  description = "CIDR block for the public subnet in AZ1"
   type        = string
 }
 
 variable "public_subnet_az2_cidr" {
-  description = "CIDR block for public subnet in AZ2"
+  description = "CIDR block for the public subnet in AZ2"
   type        = string
 }
 
 variable "private_subnet_az1_cidr" {
-  description = "CIDR block for private subnet in AZ1"
+  description = "CIDR block for the private subnet in AZ1"
   type        = string
 }
 
 variable "private_subnet_az2_cidr" {
-  description = "CIDR block for private subnet in AZ2"
+  description = "CIDR block for the private subnet in AZ2"
   type        = string
 }
 
 variable "iam_role_name" {
-  description = "IAM role name"
+  description = "Name for the IAM roles"
   type        = string
 }
 
 variable "eks_cluster_name" {
-  description = "Name for the EKS cluster"
+  description = "EKS cluster name"
+  type        = string
+}
+
+variable "fargate_profile_name" {
+  description = "Fargate profile name"
+  type        = string
+}
+
+variable "eks_namespace" {
+  description = "Namespace for workloads on Fargate"
   type        = string
 }
