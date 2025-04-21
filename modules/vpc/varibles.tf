063a1d85fe024d4d6655abf93d35366cbb857bec
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-22 00:13:48 +0530

    Create varibles.tf

---
+++ modules/vpc/varibles.tf
@@ -0,0 +1,39 @@
+variable "vpc_name" {
+  description = "The name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "az1" {
+  description = "Availability Zone 1"
+  type        = string
+}
+
+variable "az2" {
+  description = "Availability Zone 2"
+  type        = string
+}
+
+variable "public_subnet_az1_cidr" {
+  description = "CIDR block for public subnet in AZ1"
+  type        = string
+}
+
+variable "public_subnet_az2_cidr" {
+  description = "CIDR block for public subnet in AZ2"
+  type        = string
+}
+
+variable "private_subnet_az1_cidr" {
+  description = "CIDR block for private subnet in AZ1"
+  type        = string
+}
+
+variable "private_subnet_az2_cidr" {
+  description = "CIDR block for private subnet in AZ2"
+  type        = string
+}
