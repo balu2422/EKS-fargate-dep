d6d5d503c391ae58af864c7404d16637c82ab008
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-22 00:18:35 +0530

    Create main.tf

---
+++ modules/eks/main.tf
@@ -0,0 +1,32 @@
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = var.cluster_name
+  role_arn = var.cluster_role_arn
+
+  vpc_config {
+    subnet_ids = var.private_subnet_ids
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy_attachment]
+}
+
+resource "aws_eks_fargate_profile" "fargate_profile" {
+  cluster_name           = aws_eks_cluster.eks_cluster.name
+  fargate_profile_name   = var.fargate_profile_name
+  pod_execution_role_arn = var.fargate_pod_role_arn
+
+  subnets = var.fargate_subnet_ids
+
+  selector {
+    namespace = "default"
+  }
+
+  depends_on = [aws_eks_cluster.eks_cluster]
+}
+
+output "cluster_name" {
+  value = aws_eks_cluster.eks_cluster.name
+}
+
+output "fargate_profile_name" {
+  value = aws_eks_fargate_profile.fargate_profile.name
+}
