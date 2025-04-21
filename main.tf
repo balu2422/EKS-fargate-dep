29192dbc1d0c0c798596d81d73e8176aa5d1683d
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-22 00:33:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "./modules/vpc"
-  vpc_name             = var.vpc_name
-  vpc_cidr             = var.vpc_cidr
-  az1                  = var.az1
-  az2                  = var.az2
+  source                  = "./modules/vpc"
+  vpc_name                = var.vpc_name
+  vpc_cidr                = var.vpc_cidr
+  az1                     = var.az1
+  az2                     = var.az2
   public_subnet_az1_cidr  = var.public_subnet_az1_cidr
   public_subnet_az2_cidr  = var.public_subnet_az2_cidr
   private_subnet_az1_cidr = var.private_subnet_az1_cidr
@@ -15,21 +15,31 @@ module "vpc" {
 }
 
 module "iam" {
-  source = "./modules/iam"
-  name   = var.iam_role_name
+  source         = "./modules/iam"
+  name           = var.iam_role_name
 }
 
 module "eks" {
   source               = "./modules/eks"
-  name                 = var.eks_cluster_name
+  cluster_name         = var.eks_cluster_name
   private_subnet_ids   = [module.vpc.private_subnet_az1_id, module.vpc.private_subnet_az2_id]
   fargate_subnet_ids   = [module.vpc.private_subnet_az1_id, module.vpc.private_subnet_az2_id]
   cluster_role_arn     = module.iam.eks_cluster_role_arn
   fargate_pod_role_arn = module.iam.fargate_pod_role_arn
+  fargate_profile_name = var.fargate_profile_name
+  namespace            = var.eks_namespace
 }
 
 output "eks_cluster_name" {
   value = module.eks.cluster_name
+}
+
+output "fargate_profile_name" {
+  value = module.eks.fargate_profile_name
+}
+
+output "namespace" {
+  value = module.eks.namespace_name
 }
 
 output "vpc_id" {
@@ -42,8 +52,4 @@ output "private_subnet_az1_id" {
 
 output "private_subnet_az2_id" {
   value = module.vpc.private_subnet_az2_id
-}
-
-output "fargate_profile_name" {
-  value = module.eks.fargate_profile_name
 }
