9cd665d8c3fccfc29fbfc58cbdd707715f754a6c
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-22 00:18:53 +0530

    Create varibles.tf

---
+++ modules/eks/varibles.tf
@@ -0,0 +1,29 @@
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+}
+
+variable "cluster_role_arn" {
+  description = "EKS cluster role ARN"
+  type        = string
+}
+
+variable "private_subnet_ids" {
+  description = "Private subnet IDs for the EKS cluster"
+  type        = list(string)
+}
+
+variable "fargate_profile_name" {
+  description = "EKS Fargate profile name"
+  type        = string
+}
+
+variable "fargate_subnet_ids" {
+  description = "Subnets for Fargate profile"
+  type        = list(string)
+}
+
+variable "fargate_pod_role_arn" {
+  description = "Fargate Pod execution role ARN"
+  type        = string
+}
