80e693f66ff8169fd4ade07d702de9b2e2b688db
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-22 07:45:40 +0530

    Update main.tf

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -3,75 +3,45 @@ resource "aws_iam_role" "eks_cluster_role" {
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Action    = "sts:AssumeRole"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-        Effect    = "Allow"
-        Sid       = ""
-      },
-    ]
-  })
-
-  tags = {
-    Name = "${var.name}-eks-cluster-role"
-  }
-}
-
-resource "aws_iam_role" "fargate_pod_role" {
-  name = "${var.name}-fargate-pod-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action    = "sts:AssumeRole"
-        Principal = {
-          Service = "eks-fargate.amazonaws.com"
-        }
-        Effect    = "Allow"
-        Sid       = ""
-      },
-    ]
-  })
-
-  tags = {
-    Name = "${var.name}-fargate-pod-role"
-  }
-}
-
-resource "aws_iam_policy" "eks_cluster_policy" {
-  name        = "${var.name}-eks-cluster-policy"
-  description = "EKS Cluster policy for managing the cluster"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action   = ["eks:DescribeCluster", "eks:ListClusters"]
-        Resource = "*"
-        Effect   = "Allow"
-      },
-    ]
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
   })
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attachment" {
   role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = aws_iam_policy.eks_cluster_policy.arn
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "fargate_pod_role_policy_attachment" {
-  role       = aws_iam_role.fargate_pod_role.name
+resource "aws_iam_role" "fargate_pod_execution_role" {
+  name = "${var.name}-fargate-pod-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks-fargate-pods.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "fargate_pod_execution_policy" {
+  role       = aws_iam_role.fargate_pod_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
 }
 
-output "eks_cluster_role_arn" {
+output "cluster_role_arn" {
   value = aws_iam_role.eks_cluster_role.arn
 }
 
 output "fargate_pod_role_arn" {
-  value = aws_iam_role.fargate_pod_role.arn
+  value = aws_iam_role.fargate_pod_execution_role.arn
 }
