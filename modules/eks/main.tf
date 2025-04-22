bb6fa786d3fa44e8f2f62e073fe1a7d028be882c
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-22 10:33:49 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -34,21 +34,6 @@ resource "kubernetes_namespace" "eks_ns" {
 }
 
 
-data "aws_eks_cluster" "eks_cluster" {
-  name = aws_eks_cluster.eks_cluster.name
-}
-
-data "aws_eks_cluster_auth" "eks_cluster" {
-  name = aws_eks_cluster.eks_cluster.name
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks_cluster.token
-
-  load_config_file       = false
-}
 
 # Output the EKS Cluster name
 output "cluster_name" {
