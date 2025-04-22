6946be4eb1129197f8a8931bd3b811849bb0e5f2
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-22 09:28:24 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,3 +1,4 @@
+# EKS Cluster resource creation
 resource "aws_eks_cluster" "eks_cluster" {
   name     = var.cluster_name
   role_arn = var.cluster_role_arn
@@ -7,16 +8,10 @@ resource "aws_eks_cluster" "eks_cluster" {
   }
 }
 
-#resource "kubernetes_namespace" "eks_ns" {
-#  metadata {
-#    name = var.namespace
-# # }
-#}
-
-
+# EKS Fargate Profile resource creation
 resource "aws_eks_fargate_profile" "fargate_profile" {
   cluster_name           = aws_eks_cluster.eks_cluster.name
-  fargate_profile_name   = var.fargate_profile_name
+  fargate_profile_name   = var.fargate_profile_name  # Avoid 'eks-' prefix
   pod_execution_role_arn = var.fargate_pod_role_arn
 
   subnet_ids = var.fargate_subnet_ids  # ✅ FIXED from `subnets` to `subnet_ids`
@@ -25,17 +20,29 @@ resource "aws_eks_fargate_profile" "fargate_profile" {
     namespace = var.namespace
   }
 
-  depends_on = [aws_eks_cluster.eks_cluster]
+  depends_on = [aws_eks_cluster.eks_cluster]  # Ensure Fargate profile is created after EKS cluster
 }
 
+# Kubernetes Namespace resource creation
+resource "kubernetes_namespace" "eks_ns" {
+  metadata {
+    name = var.namespace
+  }
+
+  depends_on = [aws_eks_cluster.eks_cluster]  # Ensure namespace is created after EKS cluster
+}
+
+# Output the EKS Cluster name
 output "cluster_name" {
   value = aws_eks_cluster.eks_cluster.name
 }
 
+# Output the EKS Fargate Profile name
 output "fargate_profile_name" {
   value = aws_eks_fargate_profile.fargate_profile.fargate_profile_name
 }
 
-#output "namespace_name" {
-## value = kubernetes_namespace.eks_ns.metadata[0].name
-#}
+# Output the Kubernetes Namespace name
+output "namespace_name" {
+  value = kubernetes_namespace.eks_ns.metadata[0].name
+}
