ebe5ad782d079ec367ef0dcc973a25faf9859840
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-22 16:59:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+
+provider "kubernetes" {
+  alias                  = "with_dependency"
+  host                   = aws_eks_cluster.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks_auth.token
+}
+
+data "aws_eks_cluster_auth" "eks_auth" {
+  name = aws_eks_cluster.eks_cluster.name
+}
+
 provider "aws" {
   region = var.region
 }
