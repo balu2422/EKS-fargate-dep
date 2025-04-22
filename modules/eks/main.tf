8454910cc05677c5d7a077504fb5b3137fd0558d
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-22 10:40:54 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,3 +1,20 @@
+# Data sources to fetch EKS cluster details
+data "aws_eks_cluster" "eks_cluster" {
+  name = aws_eks_cluster.eks_cluster.name
+}
+
+data "aws_eks_cluster_auth" "eks_cluster" {
+  name = aws_eks_cluster.eks_cluster.name
+}
+
+# Configure Kubernetes provider with data from the created EKS cluster
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks_cluster.token
+  load_config_file       = false
+}
+
 # EKS Cluster resource creation
 resource "aws_eks_cluster" "eks_cluster" {
   name     = var.cluster_name
@@ -11,18 +28,16 @@ resource "aws_eks_cluster" "eks_cluster" {
 # EKS Fargate Profile resource creation
 resource "aws_eks_fargate_profile" "fargate_profile" {
   cluster_name           = aws_eks_cluster.eks_cluster.name
-  fargate_profile_name   = var.fargate_profile_name  # Avoid 'eks-' prefix
+  fargate_profile_name   = var.fargate_profile_name
   pod_execution_role_arn = var.fargate_pod_role_arn
-
-  subnet_ids = var.fargate_subnet_ids  # ✅ FIXED from `subnets` to `subnet_ids`
+  subnet_ids             = var.fargate_subnet_ids
 
   selector {
     namespace = var.namespace
   }
 
-  depends_on = [aws_eks_cluster.eks_cluster]  # Ensure Fargate profile is created after EKS cluster
+  depends_on = [aws_eks_cluster.eks_cluster]
 }
-
 
 # Kubernetes Namespace resource creation
 resource "kubernetes_namespace" "eks_namespace" {
@@ -30,22 +45,18 @@ resource "kubernetes_namespace" "eks_namespace" {
     name = var.namespace
   }
 
-  depends_on = [aws_eks_cluster.eks_cluster]  # Ensure namespace is created after EKS cluster
+  depends_on = [aws_eks_cluster.eks_cluster]
 }
 
-
-
-# Output the EKS Cluster name
+# Outputs
 output "cluster_name" {
   value = aws_eks_cluster.eks_cluster.name
 }
 
-# Output the EKS Fargate Profile name
 output "fargate_profile_name" {
   value = aws_eks_fargate_profile.fargate_profile.fargate_profile_name
 }
 
-# Output the Kubernetes Namespace name
 output "namespace_name" {
   value = kubernetes_namespace.eks_namespace.metadata[0].name
 }
