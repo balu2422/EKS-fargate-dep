f5f76ae0440fbb7708b168205bfc879fabd608df
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-22 07:47:53 +0530

    Update varibles.tf

--- varibles.tf
+++ varibles.tf
@@ -1,10 +1,10 @@
 variable "region" {
-  description = "AWS region"
+  description = "AWS region to deploy resources"
   type        = string
 }
 
 variable "vpc_name" {
-  description = "Name of the VPC"
+  description = "Name for the VPC"
   type        = string
 }
 
@@ -14,42 +14,42 @@ variable "vpc_cidr" {
 }
 
 variable "az1" {
-  description = "First Availability Zone"
+  description = "First availability zone"
   type        = string
 }
 
 variable "az2" {
-  description = "Second Availability Zone"
+  description = "Second availability zone"
   type        = string
 }
 
 variable "public_subnet_az1_cidr" {
-  description = "CIDR block for the public subnet in AZ1"
+  description = "CIDR block for public subnet in AZ1"
   type        = string
 }
 
 variable "public_subnet_az2_cidr" {
-  description = "CIDR block for the public subnet in AZ2"
+  description = "CIDR block for public subnet in AZ2"
   type        = string
 }
 
 variable "private_subnet_az1_cidr" {
-  description = "CIDR block for the private subnet in AZ1"
+  description = "CIDR block for private subnet in AZ1"
   type        = string
 }
 
 variable "private_subnet_az2_cidr" {
-  description = "CIDR block for the private subnet in AZ2"
+  description = "CIDR block for private subnet in AZ2"
   type        = string
 }
 
 variable "iam_role_name" {
-  description = "Name for the IAM roles"
+  description = "Base name for IAM roles (used in IAM module)"
   type        = string
 }
 
 variable "eks_cluster_name" {
-  description = "EKS cluster name"
+  description = "EKS Cluster name"
   type        = string
 }
 
@@ -59,6 +59,6 @@ variable "fargate_profile_name" {
 }
 
 variable "eks_namespace" {
-  description = "Namespace for workloads on Fargate"
+  description = "Namespace to deploy EKS workloads to"
   type        = string
 }
