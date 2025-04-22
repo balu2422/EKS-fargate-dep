745a3494a02a51cfe374095c0a99795a8701704b
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-22 16:57:21 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,35 +1,21 @@
-provider "kubernetes" {
-  alias                  = "with_dependency"
-  host                   = data.aws_eks_cluster.eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks_cluster.token
-}
-
-# Data sources to fetch EKS cluster details
-data "aws_eks_cluster" "eks_cluster" {
-  name = aws_eks_cluster.eks_cluster.name
-}
-
-data "aws_eks_cluster_auth" "eks_cluster" {
-  name = aws_eks_cluster.eks_cluster.name
-}
-
-# EKS Cluster
 resource "aws_eks_cluster" "eks_cluster" {
   name     = var.cluster_name
-  role_arn = var.cluster_role_arn
+  role_arn = var.eks_cluster_role_arn
 
   vpc_config {
-    subnet_ids = var.private_subnet_ids
+    subnet_ids         = var.private_subnet_ids
+    endpoint_private_access = false
+    endpoint_public_access  = true
   }
+
+  depends_on = [var.eks_cluster_role_dependencies]
 }
 
-# Fargate Profile
 resource "aws_eks_fargate_profile" "fargate_profile" {
   cluster_name           = aws_eks_cluster.eks_cluster.name
-  fargate_profile_name   = var.fargate_profile_name
-  pod_execution_role_arn = var.fargate_pod_role_arn
-  subnet_ids             = var.fargate_subnet_ids
+  fargate_profile_name   = "fargate-profile"
+  pod_execution_role_arn = var.fargate_pod_execution_role_arn
+  subnet_ids             = var.private_subnet_ids
 
   selector {
     namespace = var.namespace
@@ -38,28 +24,39 @@ resource "aws_eks_fargate_profile" "fargate_profile" {
   depends_on = [aws_eks_cluster.eks_cluster]
 }
 
-# Kubernetes Namespace
 resource "kubernetes_namespace" "eks_namespace" {
-  provider = kubernetes.with_dependency
-
   metadata {
     name = var.namespace
   }
 
-  depends_on = [
-    aws_eks_fargate_profile.fargate_profile
-  ]
+  depends_on = [aws_eks_fargate_profile.fargate_profile]
 }
 
-# Outputs
-output "cluster_name" {
-  value = aws_eks_cluster.eks_cluster.name
+resource "null_resource" "wait_for_eks" {
+  provisioner "local-exec" {
+    command = "echo Waiting for EKS cluster..."
+  }
+
+  depends_on = [aws_eks_cluster.eks_cluster]
 }
 
-output "fargate_profile_name" {
-  value = aws_eks_fargate_profile.fargate_profile.fargate_profile_name
-}
+resource "kubernetes_config_map" "aws_auth" {
+  provider = kubernetes.with_dependency
 
-output "namespace_name" {
-  value = kubernetes_namespace.eks_namespace.metadata[0].name
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapUsers = yamlencode([
+      {
+        userarn  = "arn:aws:iam::557548602903:user/devops-b"
+        username = "devops-b"
+        groups   = ["system:masters"]
+      }
+    ])
+  }
+
+  depends_on = [null_resource.wait_for_eks]
 }
