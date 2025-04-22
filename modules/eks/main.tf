753abf1c212817b676ada39814269e58c34aadba
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-22 10:14:33 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -23,6 +23,7 @@ resource "aws_eks_fargate_profile" "fargate_profile" {
   depends_on = [aws_eks_cluster.eks_cluster]  # Ensure Fargate profile is created after EKS cluster
 }
 
+
 # Kubernetes Namespace resource creation
 resource "kubernetes_namespace" "eks_ns" {
   metadata {
@@ -32,15 +33,21 @@ resource "kubernetes_namespace" "eks_ns" {
   depends_on = [aws_eks_cluster.eks_cluster]  # Ensure namespace is created after EKS cluster
 }
 
-# Kubernetes Provider configuration
-provider "kubernetes" {
-  host                   = aws_eks_cluster.eks_cluster.endpoint
-  token                  = data.aws_eks_cluster_auth.eks_cluster.token
+
+data "aws_eks_cluster" "eks_cluster" {
+  name = aws_eks_cluster.eks_cluster.name
 }
 
-# EKS Cluster authentication data
 data "aws_eks_cluster_auth" "eks_cluster" {
   name = aws_eks_cluster.eks_cluster.name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks_cluster.token
+
+  load_config_file       = false
 }
 
 # Output the EKS Cluster name
